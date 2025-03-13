Added gradient accumulation to save RAM. Fixed bug with VLM padding.

src/utils/attack.py
@@ -3,7 +3,7 @@ import torch.autograd.profiler as profiler
 from utils.embedding import EmbeddingModel, compute_embedding_loss
 from utils.vlm import VLM
 from utils.scheduler import LearningRateScheduler
-from utils.utils import print_memory_consumption
+from utils.utils import get_memory_consumption
 
 
 
@@ -14,10 +14,11 @@ def rag_attack(
         user_query: list, # but maybe string also
         target_answer: str,
         max_perturbation: float,
-        n_iter: int,
+        n_gradient_steps: int,
         print_every: int,
         lr_scheduler: LearningRateScheduler,
-        max_batch_size: int,
+        max_batch_size_per_iter: int,
+        gradient_acc_steps: int,
         lambda_emb: float,
         lambda_vlm: float,
         emb_loss_type: str,
@@ -25,11 +26,20 @@ def rag_attack(
     """
     Simulates an attack against the full RAG pipeline.
     The input image is jointly optimized w.r.t. the retriever and the VLM outputs
-    TODO: add option to optimize image w.r.t. only one objective
+    
+    NOTE:
+    1. max_batch_size_per_iter is the batch size per iteration 
+    1. effective batch_size = batch_size_per_iter * gradient_acc_steps
+    2. n_gradient_steps is the number of gradient updates
+    3. total number of iterations = n_gradient_steps * gradient_acc_steps
+
+    OBSERVATION: 
+    1. Using batch_size=1 with gradient accumulation is much more effective than using larger batch_size. No idea why?
     """
     initial_image = raw_image.clone()
     max_perturbation_pixels = max_perturbation*255
-    batch_size = min(len(user_query), max_batch_size)
+    batch_size_per_iter = min(len(user_query), max_batch_size_per_iter)
+    n_iter = n_gradient_steps * gradient_acc_steps
 
 
     # retrieval processing
@@ -40,24 +50,22 @@ def rag_attack(
     # VLM processing
     if lambda_vlm > 0:
         full_text_vlm_prompt, target_tokens = vlm.get_training_prompt(user_query, target_answer)
-        mock_images = [initial_image for _ in range(batch_size)]
+        mock_images = [initial_image for _ in range(batch_size_per_iter)]
         # TODO: maybe here create the whole model_inputs list (to save time)
 
 
     # initial values for loss
     loss_emb, loss_vlm = torch.tensor([0]).to(device), torch.tensor([0]).to(device)
+    grads = torch.zeros_like(raw_image)
 
     # attack iterations
     for i in range(n_iter):
-
-        # get learning rate from scheduler
-        lr = lr_scheduler.get_lr(i)
 
         # ensure that raw_image requires grad
         raw_image.requires_grad = True
 
         # sample minibatch
-        samples_idx = torch.randint(0, len(user_query), (batch_size,)).type(torch.LongTensor)
+        samples_idx = torch.randint(0, len(user_query), (batch_size_per_iter,)).type(torch.LongTensor)
         if lambda_emb > 0: user_query_embedding_batch = user_query_embedding[samples_idx,:]
         if lambda_vlm > 0: full_text_vlm_prompt_batch = [full_text_vlm_prompt[i] for i in samples_idx]
 
@@ -77,26 +85,48 @@ def rag_attack(
 
         # total loss function
         total_loss = lambda_emb * loss_emb + lambda_vlm * loss_vlm
-        if i==0 or i%print_every==print_every-1: 
-            print(f"Iter {i+1:4d}, Losses -> Embedding: {loss_emb.item():.8f}, VLM: {loss_vlm.item():.8f}, Total: {total_loss.item():.8f}")
-            print_memory_consumption(device)
+        if i==0 or ((i+1)/gradient_acc_steps)%print_every==0: 
+            print(f"Iter {(i//gradient_acc_steps)+1:4d}/{n_gradient_steps}, RAM usage -> {get_memory_consumption(device):.2f} GB, Losses -> Embedding: {loss_emb.item():.8f}, VLM: {loss_vlm.item():.8f}, Total: {total_loss.item():.8f}")
 
         # backpropagation
-        grads = torch.autograd.grad(total_loss, raw_image)
+        grads += torch.autograd.grad(total_loss, raw_image)[0]
 
-        # optimization step
-        with torch.no_grad():
-            # take step
-            raw_image -= lr * torch.sign(grads[0])
-    
-            # clip according to attack budget
-            torch.clip(raw_image, min=initial_image-max_perturbation_pixels, max=initial_image+max_perturbation_pixels, out=raw_image)
-            # clip to make sure we stay within allowed RGB values
-            torch.clip(raw_image, min=0, max=255, out=raw_image)
+        if (i+1)%gradient_acc_steps == 0:
+            # compute average gradient
+            grads /= gradient_acc_steps
 
+            # get learning rate from scheduler
+            lr = lr_scheduler.get_lr(i//gradient_acc_steps)
+            
+            # optimization step
+            with torch.no_grad():
+                raw_image = attack_step_bim(raw_image, grads, lr, max_perturbation_pixels, initial_image)
+            
+            # zero the gradient
+            grads = torch.zeros_like(raw_image)
+            
         # -- stop indenting here and uncomment next line to profile timing issues --
         # print(prof.key_averages().table(sort_by="cpu_time_total"))
         
+    return raw_image
+
+
+def attack_step_bim(
+        raw_image: torch.tensor, 
+        grads: torch.tensor, 
+        lr: float, 
+        max_perturbation_pixels: int, 
+        initial_image: torch.tensor):
+    """
+    Implement the basic iterative method attack (FGSM but iterative)
+    """
+    # take step
+    raw_image -= lr * torch.sign(grads)
+    # clip according to attack budget
+    torch.clip(raw_image, min=initial_image-max_perturbation_pixels, max=initial_image+max_perturbation_pixels, out=raw_image)
+    # clip to make sure we stay within allowed RGB values
+    torch.clip(raw_image, min=0, max=255, out=raw_image)
+    
     return raw_image
 
 
@@ -113,12 +143,13 @@ if __name__ == "__main__":
     image = load_image("https://farm9.staticflickr.com/8096/8445896722_e28fb3f055_z.jpg")
     device = get_device(prefer_mps=True)
     max_perturbation = 0.05
-    n_iter = 100
+    n_gradient_steps = 100
+    gradient_acc_steps = 4
     lr_scheduler = LearningRateScheduler(start_lr=255 * (5e-3), end_lr=255*(5e-4), n_iter=n_iter) # multiply by 255 since input is [0,255]
     lambda_emb = 1
     lambda_vlm = 0
     print_every = 10
-    max_batch_size = 10 # number of queries to optimize for simulataneously (actual batch size is min(this, len([user_query])))
+    max_batch_size_per_iter = 10 # number of queries to optimize for simulataneously (actual batch size is min(this, len([user_query])))
     emb_loss_type = "mse" # mse, l2, l2_nosqrt, cos
     print("Initialized variables!")
 
@@ -139,10 +170,11 @@ if __name__ == "__main__":
         user_query=user_query,
         target_answer=target_answer,
         max_perturbation=max_perturbation,
-        n_iter=n_iter,
+        n_gradient_steps=n_gradient_steps,
         print_every=print_every,
         lr_scheduler=lr_scheduler,
-        max_batch_size=max_batch_size,
+        max_batch_size_per_iter=max_batch_size_per_iter,
+        gradient_acc_steps=gradient_acc_steps,
         lambda_emb=lambda_emb,
         lambda_vlm=lambda_vlm,
         emb_loss_type=emb_loss_type,

src/utils/vlm.py
@@ -146,22 +146,26 @@ class VLM():
         return generated_texts[0]
 
     def forward(self, image, mock_images, prompt, overwrite: bool = False):
+        """
+        Important:
+        padding_side should be set to "left", otherwise this will interfere with the attack optimization
+        """
         self.model.eval()
         if isinstance(prompt, str): prompt = [prompt]
         if self.name in SMOL_VLMS:
             if overwrite:
-                inputs_vlm = self.processor(text=prompt, images=mock_images, return_tensors="pt", truncation=True, padding=True).to(self.device) # here we feed the intiial image since we are overwriting it anyways
+                inputs_vlm = self.processor(text=prompt, images=mock_images, return_tensors="pt", truncation=True, padding=True, padding_side="left").to(self.device) # here we feed the intiial image since we are overwriting it anyways
                 image_ppd_vlm = process_image(image, self)
                 inputs_vlm['pixel_values'] = image_ppd_vlm.unsqueeze(0).unsqueeze(0).repeat(len(prompt),1,1,1,1).to(self.device)
             else:
-                inputs_vlm = self.processor(text=prompt, images=[image for _ in range(len(prompt))], return_tensors="pt", truncation=True, padding=True).to(self.device) # here we feed the intiial image since we are overwriting it anyways
+                inputs_vlm = self.processor(text=prompt, images=[image for _ in range(len(prompt))], return_tensors="pt", truncation=True, padding=True, padding_side="left").to(self.device) # here we feed the intiial image since we are overwriting it anyways
         
         if self.name == "Qwen/Qwen2.5-VL-3B-Instruct":
-            # it seems that qwen implement their preprocessors in pytorch --> differentiable (no need to overwrite image)
-            inputs_vlm = self.processor(text=prompt, images=[image for _ in range(len(prompt))], return_tensors="pt", truncation=True, padding=True).to(self.device)
+            # it seems that qwen implements their preprocessors in pytorch --> differentiable (no need to overwrite image)
+            inputs_vlm = self.processor(text=prompt, images=[image for _ in range(len(prompt))], return_tensors="pt", truncation=True, padding=True, padding_side="left").to(self.device)
         
         if inputs_vlm:
-            out = self.model(**inputs_vlm)
+            out = self.model(**inputs_vlm, use_cache=False, output_attentions=False, output_hidden_states=False)
             return out
 
         quit(f"Not supported model {self.name}!")
@@ -169,8 +173,6 @@ class VLM():
 
 
     def compute_gen_loss(self, vlm_output, target_tokens):
-        # TODO: not sure if we need to pass logits to softmax first
-        # if self.name == "HuggingFaceTB/SmolVLM-256M-Instruct":
         logits_to_optimize = vlm_output.logits[:,-len(target_tokens)-1:-1,:].transpose(1,2)
         target_tokens = target_tokens.unsqueeze(0).repeat(logits_to_optimize.shape[0], 1)
         return torch.nn.CrossEntropyLoss()(logits_to_optimize, target_tokens)