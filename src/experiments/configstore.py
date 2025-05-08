8ab239ad7d56037c3bd1142e2856855018f7c14b
Ezzeldin <ezzeldin.shereen.zaki@gmail.com>
2025-05-08 13:59:03 +0100

    create default dev experiment

--- src/experiments/configstore.py
+++ src/experiments/configstore.py
@@ -16,7 +16,7 @@ from wrappers.vlm import VLMName
 from wrappers.dataset import DatasetName
 
 
-DEFAULT_EXPERIMENT = "testing"
+DEFAULT_EXPERIMENT = "dev"
 
 configstore = ConfigStore.instance()
 
@@ -35,10 +35,10 @@ def get_config_name() -> str:
 
 
 """
-testing Configuration
+Development Configuration (default)
 """
 configstore.store(
-    name="testing",
+    name="dev",
     node=ExperimentConfig(
         train=ExperimentTrainConfig(
             embedder_list=[EmbedderName.CLIP_BASE_PATCH16],
@@ -46,6 +46,29 @@ configstore.store(
             gen_topk_list=[1],
             print_every=2,
             n_gradient_steps=50,
+        ),
+        eval=ExperimentEvalConfig(
+            gen_topk_list=[-1],
+        )
+    ),
+)
+
+"""
+testing Configuration
+"""
+configstore.store(
+    name="testing",
+    node=ExperimentConfig(
+        train=ExperimentTrainConfig(
+            embedder_list=[EmbedderName.CLIP_BASE_PATCH16],
+            vlm_list=[VLMName.SMOLVLM_1_256M],
+            gen_topk_list=[1],
+            is_targeted=True,
+            target_query_idx=[2],
+            n_knn_target_queries=5,
+            print_every=2,
+            n_gradient_steps=10,
+            optimize_nontargeted_queries_list=[True,False]
         ),
         eval=ExperimentEvalConfig(
             gen_topk_list=[-1],
