ef2c57c36f739b206a086bca829635d747489d44
Burak Hasircioglu <burakhasircioglu@gmail.com>
2025-05-20 23:27:10 +0100

    UMAP visualization notebook added (#43)
    * UMAP visualization notebook added
    
    * Use the same model implementations for umap visualtions
    
    * Add instructions to Readme for optional deps
    
    ---------
    
    Co-authored-by: Dan Ristea <danristea@protonmail.com>

--- src/config/__init__.py
+++ src/config/__init__.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 
 DATA_FOLDER = Path(__file__).parents[2] / "data"
+OUTPUTS_FOLDER = Path(__file__).parents[2] / "outputs"
 ATTACKS_FOLDER = DATA_FOLDER / "attacks" / "draft"
 EMBEDDINGS_FOLDER = DATA_FOLDER / "embeddings"
 RESULTS_FOLDER = DATA_FOLDER / "results" / "draft"
